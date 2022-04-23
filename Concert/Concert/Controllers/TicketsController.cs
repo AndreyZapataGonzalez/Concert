371fa2a0a428ff86db2adeae30873e8a6ae66ec7
Andrey_Zapata <andreyzapata224925@correo.itm.edu.co>
2022-04-22 22:41:14 -0500

    Agregar vistas en validaciones, falta implementar combos

--- Concert/Concert/Controllers/TicketsController.cs
+++ Concert/Concert/Controllers/TicketsController.cs
@@ -53,10 +53,14 @@ namespace Concert.Controllers
         // POST: Tickets/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        /*[HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,WasUsed,Document,Name,Date")] Ticket ticket)
+        public async Task<IActionResult> Create(int? Id)
         {
+            if (Id == null || Id > 5000 || Id <1)
+            {
+                return NotFound();
+            }
             if (ModelState.IsValid)
             {
                 _context.Add(ticket);
@@ -64,30 +68,80 @@ namespace Concert.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(ticket);
-        }
+        }*/
 
         // GET: Tickets/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        /*public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
+            if (id == null || id > 5000 || id < 1)
             {
                 return NotFound();
             }
 
-            var ticket = await _context.Tickets.FindAsync(id);
+            Ticket ticket = await _context.Tickets.FindAsync(id);
             if (ticket == null)
             {
                 return NotFound();
             }
-            return View(ticket);
+
+            if (ticket.WasUsed == false)
+            {
+                return View(ticket);
+            }
+
+            else
+            {
+                return NotFound();
+            }
+            
+        }*/
+
+        public IActionResult Edit()
+        {
+            return View();
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id >= 5000 || id < 0)
+            {
+                return RedirectToAction("InvalidTicket", "Tickets");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Ticket ticket = await _context.Tickets.FindAsync(id);
+                try
+                {
+                    if (ticket.WasUsed == true)
+                    {
+                        throw new HttpRequestException("Ese Ticket ya fue usado por: " +ticket.Name + " en la fecha: " + ticket.Date + " su ingreso fue por: "+ticket.Entrance.Description);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Test", "Tickets");
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
         }
 
         // POST: Tickets/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        /*[HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,WasUsed,Document,Name,Date")] Ticket ticket)
+        public async Task<IActionResult> Edit(int id,Ticket ticket)
         {
             if (id != ticket.Id)
             {
@@ -115,7 +169,7 @@ namespace Concert.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(ticket);
-        }
+        }*/
 
         // GET: Tickets/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -149,6 +203,16 @@ namespace Concert.Controllers
         private bool TicketExists(int id)
         {
             return _context.Tickets.Any(e => e.Id == id);
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult InvalidTicket()
+        {
+            return View();
         }
     }
 }
